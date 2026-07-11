aa15dc3b071dca386baacbd1d0b43d92843648c1
Alejandro Orta <orta.sanz.93@gmail.com>
2026-07-11 18:20:41 +0200

    chore: añade configuración de prettier, reformatea el código y actualiza dependencias
    Se introduce .prettierrc/.prettierignore con los scripts format/format:check,
    se aplica el formateo en el código fuente, estilos y utilidades del scraper,
    y se actualizan varias dependencias (react, babel, axios, cheerio, sass, webpack, etc).

--- utils/nodeScraper/script.js
+++ utils/nodeScraper/script.js
@@ -5,28 +5,30 @@ const getRecipesInPage = require('./modules/recipesInPage');
 const getRecipeData = require('./modules/recipeData');
 
 const getAndSaveAllRecipes = async () => {
-    const recipes = [];
-    const totalPages = await getTotalRecipesPages();
+  const recipes = [];
+  const totalPages = await getTotalRecipesPages();
 
-    for (let page = 1; page <= totalPages; page++) {
-        console.log(`--- Getting recipes from page ${page}/${totalPages} ---`);
-        const pageRecipes = await getRecipesInPage(page);
+  for (let page = 1; page <= totalPages; page++) {
+    console.log(`--- Getting recipes from page ${page}/${totalPages} ---`);
+    const pageRecipes = await getRecipesInPage(page);
 
-        console.log(`--- Map recipes with more data ---`);
-        const mappedRecipes = await Promise.all(pageRecipes.map(getRecipeData)).then((recipesData) => {
-            return pageRecipes.map((recipe, key) => ({
-                ...recipe,
-                ...recipesData[key]
-            }));
-        });
-
-        console.log(`--- Saved ${mappedRecipes.length} ---\n`);
-        recipes.push(...mappedRecipes);
-    };
-
-    fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes), {
-        flag: 'w+'
+    console.log(`--- Map recipes with more data ---`);
+    const mappedRecipes = await Promise.all(
+      pageRecipes.map(getRecipeData)
+    ).then((recipesData) => {
+      return pageRecipes.map((recipe, key) => ({
+        ...recipe,
+        ...recipesData[key],
+      }));
     });
-}
+
+    console.log(`--- Saved ${mappedRecipes.length} ---\n`);
+    recipes.push(...mappedRecipes);
+  }
+
+  fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes), {
+    flag: 'w+',
+  });
+};
 
 getAndSaveAllRecipes();
